d17d9026c6842f9ef7a3d441ca3314e875e97b5c
unknown <delgadojustin100@yahoo.com>
2022-12-22 22:01:34 -0500

    almost gave up, I reEvaluated everything and a typo almost ruined everything

--- script.js
+++ script.js
@@ -5,60 +5,64 @@ const numbercharacter = ["0,1,2,3,4,5,6,7,8,9"];
 const uppercase = ["Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M"];
 const lowercase = ["q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,n,m"];
 const emojis = ["😊,😂,🤣,❤️,😒,👌,💕,😁,👍,🙌,🤦‍♀️,🤦‍♂️,🤷‍♀️,🤷‍♂️,✌️,🤞,😉,😎,🎶,💖,😢,😜,😶‍🌫️,🥶,🥵,😱,😵‍💫,🥴,😠,😡"]
- 
-function getPasswordOptions() {
-  var theword = ' ';
-  for (var i = 0; i < num; i++)
+var options ="0,1,2,3,4,5,6,7,8,9";
 
-}; 
+//
+var passw = " ";
 function generatePassword() {
+  passwordtxt = document.querySelector("#password")
+  passwordtxt.value = " ";
+  var passwordlength =  window.prompt("How long do you want your password? (8-128 characters long)"); 
+  console.log(passwordlength);
+// addEventListener("click", generateBtn);
+// window.prompt("how long");
 
-var newPass = function(num, str) {
-  
+if (passwordlength < 8 || passwordlength > 128) {
 
-  };
-
-return theword;
-};
-
-return newPass(setpasswordlength(), makepassword());
-
-
-function writePassword() {
-var passwordText = document.querySelector("#password");
-var thepass = generatePassword();
-passwordText.value = password;
+  alert("Password has to be over 8 & under 128")
 }
+var hasSpecialCharacters = confirm(
+  'Click OK to confirm including special characters.'
+);
+var hasNumericCharacters = confirm(
+  'Click OK to confirm including numeric characters.'
+);
+var hasLowercaseCharacters = confirm(
+  'Click OK to confirm including lowercase characters.'
+);
+var hasUppercaseCharacters = confirm(
+  'Click OK to confirm including uppercase characters.'
+);
+var hasEmojiCharacters = confirm(
+  'Click OK to confirm including emoji characters.'
+);
+
+for (let i = 0; i < length; i++) {
+ 
+
+};
+};
+ 
+ 
+// return newPass(setpasswordlength(), makepassword());
+// getPasswordOptions();
+
+
 
 function setpasswordlength() {
-  var thlength = 0;
-
-
-
-
+  // var thlength = 0;
 
 }
 // console.log(passwordlength);
 //variable to
-passwordText.addEventListener("click", passwordText)
-if(password < 8 || passwordlength > 128); {
-alert("The password needs to be longer than 8, and lower than 128")
-}
+// passwordText.addEventListener("click", passwordText)
+// if(password < 8 || passwordlength > 128); {
+// alert("The password needs to be longer than 8, and lower than 128")
 
-
-
-// var hasSpecialCharacters = 
-
-// var passwordOptions = 
-
-
-
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-
-
-
+  // var newPass = function(num, str) {
+    
+  //   };
+  // return theword;
+// getPasswordOptions();
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
